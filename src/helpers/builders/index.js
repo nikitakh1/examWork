768ac09a571c6cc9120c3b03db226b411d4cfbd3
nikitakh1 <nikitakh1@users.noreply.github.com>
2024-12-17 19:31:45 +0000

    deploy: 9e12ab9e3c89074ae2c26a1c0fd18409574bc380

--- src/helpers/builders/index.js
+++
@@ -1,2 +0,0 @@
-export * from './user.builder';
-export * from './article.builder';
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export * from './main.page';
-export * from './register.page'
-export * from './settings.page'
-export * from './article.page'
